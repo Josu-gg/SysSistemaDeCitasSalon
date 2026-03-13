7e65eb44ea1f991e43d78cbf8451ab5e7eb0b9ab
JLiz23 <jencyfranco682@gmail.com>
2026-03-12 21:40:00 -0600

    Agregue anotaciones de la clase Categoria y Persona de EN

--- SysSistemaDeCitasSalon.EN/Categoria.cs
+++ SysSistemaDeCitasSalon.EN/Categoria.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace SysSistemaDeCitasSalon.EN
 {
     public class Categoria
     {
+        [Key]
         public int IdCategoria { get; set; }
+        [Required(ErrorMessage ="El nombre de la categoria es obligatorio")]
+        [StringLength(100, ErrorMessage = "El nombre de la categoría no puede tener más de 100 caracteres.")]
         public string NombreCategoria { get; set; }
+        [Required]
         public bool Estado { get; set; }
     }
 }
--- SysSistemaDeCitasSalon.EN/Persona.cs
+++ SysSistemaDeCitasSalon.EN/Persona.cs
@@ -3,16 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace SysSistemaDeCitasSalon.EN
 {
     public class Persona
     {
+        [Key]
         public int IdPersona { get; set; }
-        public string Nombre { get; set; } 
-        public string Apellido { get; set; } 
-        public string Telefono { get; set; } 
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        [StringLength(50, ErrorMessage = "El nombre no puede tener más de 50 caracteres.")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "El apellido es obligatorio.")]
+        [StringLength(50, ErrorMessage = "El apellido no puede tener más de 50 caracteres.")]
+        public string Apellido { get; set; }
+        [Required(ErrorMessage = "El teléfono es obligatorio.")]
+        [Phone(ErrorMessage = "Ingrese un número de teléfono válido.")]
+        public string Telefono { get; set; }
+        [StringLength(150, ErrorMessage = "La dirección no puede tener más de 150 caracteres.")]
         public string Direccion { get; set; }
+        [Required(ErrorMessage = "El DUI es obligatorio.")]
+        [StringLength(10, ErrorMessage = "El DUI debe tener máximo 10 caracteres.")]
         public string DUI { get; set; }
     }
 }
