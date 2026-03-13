70edc4350baaff3e2522df62267c5c58bffc4fdb
Elmer Portillo <Elmerportillo2005@outlook.com>
2026-03-12 21:21:44 -0600

    Agregue notaciones de datos a las clase de producto y servicio de la EN

--- SysSistemaDeCitasSalon.EN/Producto.cs
+++ SysSistemaDeCitasSalon.EN/Producto.cs
@@ -3,16 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.ComponentModel.DataAnnotations;
 namespace SysSistemaDeCitasSalon.EN
 {
     public class Producto
     {
+        [Key]
+  
         public int IdProducto { get; set; }
-        public string NombreProducto { get; set; } 
+        [Required(ErrorMessage = " El Nombre del producto es obligatorio")]
+        [StringLength(100, ErrorMessage = " El Nombre del producto no puede tener más de 100 caracteres")]
+        public string NombreProducto { get; set; }
+        [StringLength(500, ErrorMessage = " La Descripción del producto no puede tener más de 500 caracteres")]
         public string? Descripcion { get; set; }
+            [Required(ErrorMessage = " El Precio del producto es obligatorio")]
+            [Range(0.01, double.MaxValue, ErrorMessage = " El Precio del producto debe ser un valor positivo")]
         public decimal Precio { get; set; }
+            [Required(ErrorMessage = " El Stock del producto es obligatorio")]
+            [Range(0, int.MaxValue, ErrorMessage = " El Stock del producto no puede ser negativo")]
         public int Stock { get; set; }
+
         public int IdCategoria { get; set; }
         public int IdEstado { get; set; }
     }
--- SysSistemaDeCitasSalon.EN/Servicio.cs
+++ SysSistemaDeCitasSalon.EN/Servicio.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,25 @@ namespace SysSistemaDeCitasSalon.EN
 {
     public class Servicio
     {
+        [Key]
         public int IdServicio { get; set; }
+        [Required(ErrorMessage = "El nombre del servicio es obligatorio.")]
+        [StringLength(100, ErrorMessage = "El nombre del servicio no puede tener más de 100 caracteres.")]
         public string NombreServicio { get; set; }
+        [Required(ErrorMessage = "La descripción del servicio es obligatoria.")]
+        [StringLength(500, ErrorMessage = "La descripción del servicio no puede tener más de 500 caracteres.")]
         public string? Descripcion { get; set; }
+        [Required(ErrorMessage = "El precio del servicio es obligatorio.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "El precio del servicio debe ser un valor positivo.")]
         public decimal Precio { get; set; }
+        [Required(ErrorMessage = "La duración del servicio es obligatoria.")]
+     
         public TimeSpan DuracionMinutos { get; set; }
+        [Required(ErrorMessage = "La categoría del servicio es obligatoria.")]
+
         public int IdCategoria { get; set; }
+        [Required(ErrorMessage = "El estado del servicio es obligatorio.")]
+       
         public int IdEstado { get; set; }
     }
 }
